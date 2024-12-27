68628591d58f3e38b0a438c8b5fceb296f995758
Lil-Muqorrobien <yayasanlmqalislami@gmail.com>
2024-12-28 03:56:28 +0700

    Update mencoba.js

--- mencoba.js
+++ mencoba.js
@@ -1,29 +1,29 @@
-document.write('');
-document.write('<style> ');
-document.write('   img.publikasi-kegiatan-thumb{padding:2px;width:100px;height:100px;float:left;margin:0px 10px 10px;border: 1px solid #69B7E2;}');
-document.write('  .publikasi-kegiatan-container {font-family:$(\'Poppins\', sans-serif);font-size:12px;font-style:normal;}');
-document.write('  ul.publikasi-kegiatan-container li{list-style-type: none; margin-bottom: 10px;font-size:12px;float:left;width:100%}');
-document.write('  ul.publikasi-kegiatan-container {counter-reset: countposts;list-style-type:none;padding:0;}');
-document.write('  ul.publikasi-kegiatan-container li:before {content: font-size: 13px;font-weight: 700px;color: #000000;background: #69B7E2;line-height:25px;height:25px;width:25px;text-align:center;-webkit-border-radius: 50%;-moz-border-radius: 50%;border-radius: 50%;}');
-document.write('  .publikasi-kegiatan-container a{text-decoration:normal;}');
-document.write('  .publikasi-kegiatan-title {margin-bottom:5px;}');
-document.write('  .publikasi-kegiatan-title a {font-size:20px; color: #006400;font-family:$(\'Poppins\', sans-serif;)}');
-document.write('  .publikasi-kegiatan-details {margin: 5px 0px 0px 92px;font-size:14px;}');
-document.write('  .publikasi-kegiatan-details a{background-color: #cdf3f2;color: #006400;}   ');
-document.write('</style>');
-document.write('');
-document.write('<ul class="publikasi-kegiatan-container">');
-document.write('<li class="publikasi-kegiatan-list">');
-document.write('<img class="publikasi-kegiatan-thumb" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiPAd4mcZohgc_kNDcKfr5rs8VaWevVJfAHIqt-5mNV3DpoGuXl4eKqq-7Mg78WFPogzQM9U_kDogDzFcIRerJUc-52-9armtGRRA-q2wYgNQEX0NkvTvxXQSBoBSISxWpnrvS3D6VATDm1uUAzpvhcyHaSvZnLmMZJH8rNGhHZIPHfNeoaw1Zpe9u7Gn-0/s710/RANI.png" />');
-document.write('');
-document.write('<div class="publikasi-kegiatan-title">');
-document.write('<a target="_top">Ummi Fauziah Rani, S.Pd</a>');
-document.write('</div>');
-document.write('Photo-Photo Kegiatan Pramuka Para Santri Pondok Pesantren Lil-Muqorrobien Al-Islami. <br />');
-document.write('<div class="publikasi-kegiatan">');
-document.write('_________________________________<br />');
-document.write('25 Desember 2024 | Gabung Makmur<br /> ');
-document.write('Guru Aktif Pon. Pes. Lil-Muqorrobien');
-document.write('</div>');
-document.write('</li>');
-document.write('</ul>');
+
+<style> 
+   img.publikasi-kegiatan-thumb{padding:2px;width:100px;height:100px;float:left;margin:0px 10px 10px;border: 1px solid #69B7E2;}
+  .publikasi-kegiatan-container {font-family:$('Poppins', sans-serif);font-size:12px;font-style:normal;}
+  ul.publikasi-kegiatan-container li{list-style-type: none; margin-bottom: 10px;font-size:12px;float:left;width:100%}
+  ul.publikasi-kegiatan-container {counter-reset: countposts;list-style-type:none;padding:0;}
+  ul.publikasi-kegiatan-container li:before {content: font-size: 13px;font-weight: 700px;color: #000000;background: #69B7E2;line-height:25px;height:25px;width:25px;text-align:center;-webkit-border-radius: 50%;-moz-border-radius: 50%;border-radius: 50%;}
+  .publikasi-kegiatan-container a{text-decoration:normal;}
+  .publikasi-kegiatan-title {margin-bottom:5px;}
+  .publikasi-kegiatan-title a {font-size:20px; color: #006400;font-family:$('Poppins', sans-serif;)}
+  .publikasi-kegiatan-details {margin: 5px 0px 0px 92px;font-size:14px;}
+  .publikasi-kegiatan-details a{background-color: #cdf3f2;color: #006400;}   
+</style>
+
+<ul class="publikasi-kegiatan-container">
+<li class="publikasi-kegiatan-list">
+<img class="publikasi-kegiatan-thumb" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiPAd4mcZohgc_kNDcKfr5rs8VaWevVJfAHIqt-5mNV3DpoGuXl4eKqq-7Mg78WFPogzQM9U_kDogDzFcIRerJUc-52-9armtGRRA-q2wYgNQEX0NkvTvxXQSBoBSISxWpnrvS3D6VATDm1uUAzpvhcyHaSvZnLmMZJH8rNGhHZIPHfNeoaw1Zpe9u7Gn-0/s710/RANI.png" />
+
+<div class="publikasi-kegiatan-title">
+<a target="_top">Ummi Fauziah Rani, S.Pd</a>
+</div>
+Photo-Photo Kegiatan Pramuka Para Santri Pondok Pesantren Lil-Muqorrobien Al-Islami. <br />
+<div class="publikasi-kegiatan">
+_________________________________<br />
+25 Desember 2024 | Gabung Makmur<br /> 
+Guru Aktif Pon. Pes. Lil-Muqorrobien
+</div>
+</li>
+</ul>
