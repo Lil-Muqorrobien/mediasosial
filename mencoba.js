7fe583a14166a76296d0ddfd5cffd0261e769be9
Lil-Muqorrobien <yayasanlmqalislami@gmail.com>
2024-12-28 04:05:52 +0700

    Update mencoba.js

--- mencoba.js
+++ mencoba.js
@@ -1,29 +1,185 @@
+   .authorlmq:after {
+    position: absolute;
+    top: -0 !important;
+    left: 0;
+    content: " ";
+    
+    background-size: 100% 100px;
+    width: 100%;
+    height: 100px;
+    float: left;
+    z-index: 99;
+}
 
-<style> 
-   img.publikasi-kegiatan-thumb{padding:2px;width:100px;height:100px;float:left;margin:0px 10px 10px;border: 1px solid #69B7E2;}
-  .publikasi-kegiatan-container {font-family:$('Poppins', sans-serif);font-size:12px;font-style:normal;}
-  ul.publikasi-kegiatan-container li{list-style-type: none; margin-bottom: 10px;font-size:12px;float:left;width:100%}
-  ul.publikasi-kegiatan-container {counter-reset: countposts;list-style-type:none;padding:0;}
-  ul.publikasi-kegiatan-container li:before {content: font-size: 13px;font-weight: 700px;color: #000000;background: #69B7E2;line-height:25px;height:25px;width:25px;text-align:center;-webkit-border-radius: 50%;-moz-border-radius: 50%;border-radius: 50%;}
-  .publikasi-kegiatan-container a{text-decoration:normal;}
-  .publikasi-kegiatan-title {margin-bottom:5px;}
-  .publikasi-kegiatan-title a {font-size:20px; color: #006400;font-family:$('Poppins', sans-serif;)}
-  .publikasi-kegiatan-details {margin: 5px 0px 0px 92px;font-size:14px;}
-  .publikasi-kegiatan-details a{background-color: #cdf3f2;color: #006400;}   
-</style>
+.authorlmq {
+    min-height: 375px;
+    position: relative;
+   
+    padding-top: 50px;
+    padding-bottom: 50px;
+    background-position: center;
+        background-size: cover;
+}
+#authorlmq .carousel-inner:hover{
+  cursor: -moz-grab;
+  cursor: -webkit-grab;
+}
+#authorlmq .carousel-inner:active{
+  cursor: -moz-grabbing;
+  cursor: -webkit-grabbing;
+}
+#authorlmq .carousel-inner .item{
+  overflow: hidden;
+}
 
-<ul class="publikasi-kegiatan-container">
-<li class="publikasi-kegiatan-list">
-<img class="publikasi-kegiatan-thumb" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiPAd4mcZohgc_kNDcKfr5rs8VaWevVJfAHIqt-5mNV3DpoGuXl4eKqq-7Mg78WFPogzQM9U_kDogDzFcIRerJUc-52-9armtGRRA-q2wYgNQEX0NkvTvxXQSBoBSISxWpnrvS3D6VATDm1uUAzpvhcyHaSvZnLmMZJH8rNGhHZIPHfNeoaw1Zpe9u7Gn-0/s710/RANI.png" />
+.authorlmq_indicators .carousel-indicators{
+  left: 0;
+  margin: 0;
+  width: 100%;
+  font-size: 0;
+  height: 20px;
+  bottom: 15px;
+  padding: 0 5px;
+  cursor: e-resize;
+  overflow-x: auto;
+  overflow-y: hidden;
+  position: absolute;
+  text-align: center;
+  white-space: nowrap;
+}
+.authorlmq_indicators .carousel-indicators li{
+  padding: 0;
+  width: 14px;
+  height: 14px;
+  border: none;
+  text-indent: 0;
+  margin: 2px 3px;
+  cursor: pointer;
+  display: inline-block;
+  background: #ffffff;
+  -webkit-border-radius: 100%;
+  border-radius: 100%;
+}
+.authorlmq_indicators .carousel-indicators .active{
+  padding: 0;
+  width: 14px;
+  height: 14px;
+  border: none;
+  margin: 2px 3px;
+  background-color: #9dd3af;
+  -webkit-border-radius: 100%;
+  border-radius: 100%;
+}
+.authorlmq_indicators .carousel-indicators::-webkit-scrollbar{
+  height: 3px;
+}
+.authorlmq_indicators .carousel-indicators::-webkit-scrollbar-thumb{
+  background: #eeeeee;
+  -webkit-border-radius: 0;
+  border-radius: 0;
+}
 
-<div class="publikasi-kegiatan-title">
-<a target="_top">Ummi Fauziah Rani, S.Pd</a>
-</div>
-Photo-Photo Kegiatan Pramuka Para Santri Pondok Pesantren Lil-Muqorrobien Al-Islami. <br />
-<div class="publikasi-kegiatan">
-_________________________________<br />
-25 Desember 2024 | Gabung Makmur<br /> 
-Guru Aktif Pon. Pes. Lil-Muqorrobien
-</div>
-</li>
-</ul>
+.authorlmq_control_button .carousel-control{
+  top: 175px;
+  opacity: 1;
+  width: 40px;
+  bottom: auto;
+  height: 40px;
+  font-size: 10px;
+  cursor: pointer;
+  font-weight: 700;
+  overflow: hidden;
+  line-height: 38px;
+  text-shadow: none;
+  text-align: center;
+  position: absolute;
+  background: transparent;
+  border: 2px solid #ffffff;
+  text-transform: uppercase;
+  -webkit-border-radius: 100%;
+  border-radius: 100%;
+  -webkit-box-shadow: none;
+  box-shadow: none;
+  -webkit-transition: all 0.6s cubic-bezier(0.3,1,0,1);
+  transition: all 0.6s cubic-bezier(0.3,1,0,1);
+}
+
+.authorlmq_header{
+  top: 0;
+  left: 0;
+  bottom: 0;
+  width: 550px;
+  display: block;
+  margin: 30px auto;
+  text-align: center;
+  position: relative;
+}
+.authorlmq_header h4{
+  color: #000000;
+  font-size: 30px;
+  font-weight: 600;
+  position: relative;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+}
+
+.authorlmq_slide{
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 70%;
+  margin: auto;
+  padding: 0px;
+  position: relative;
+  text-align: center;
+}
+.authorlmq_slide img {
+    top: 0;
+    left: 0;
+    right: 0;
+    width: 70px;
+    height: 136px;
+    margin: auto;
+    display: block;
+    color: #f2f2f2;
+    font-size: 18px;
+    line-height: 46px;
+    text-align: center;
+    position: relative;
+    border-radius: 50%;
+    box-shadow: -6px 6px 6px rgba(0, 0, 0, 0.23);
+    -moz-box-shadow: -6px 6px 6px rgba(0, 0, 0, 0.23);
+    -o-box-shadow: -6px 6px 6px rgba(0, 0, 0, 0.23);
+    -webkit-box-shadow: -6px 6px 6px rgba(0, 0, 0, 0.23);
+}
+ 
+.authorlmq_slide p {
+    color: #000000;
+    font-size: 14px;
+    line-height: 1.4;
+    margin: 12px 0 20px 0;
+}
+.authorlmq_slide h4 {
+  color: #000000;
+  font-size: 16px;
+  margin: 10px 0 13px 0;
+}
+
+.authorlmq .carousel {
+	padding-bottom:50px;
+}
+.authorlmq .carousel-control-next-icon, .testimonial .carousel-control-prev-icon {
+    width: 35px;
+    height: 35px;
+}
+.mediaonlinenyalmq{
+   width : 100px;
+   top: 0px;
+   left: 0px;
+   right: 0px;
+   margin: auto;
+   display: flex;
+   border-radius: 50%;
+   padding: 0px 0px 12px 0px;
+   }
